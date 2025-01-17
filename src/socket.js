7679823b8f8a9b1c87e03e9777943de83b69b973
S-Kulibaba <skulibaba053@gmail.com>
2025-01-17 19:47:02 +0100

    .env isolated

--- src/socket.js
+++ src/socket.js
@@ -4,75 +4,78 @@ let onDisconnectCallback;
 let isConnected = false;
 let messageQueue = [];
 
+const SERVER_URL = process.env.REACT_APP_WS_SERVER_URL || "ws://localhost:8080";
+console.log(SERVER_URL);
+
 export const connectToServer = () => {
-    return new Promise((resolve, reject) => {
-        socket = new WebSocket('ws://localhost:8080');
-        
-        socket.onopen = () => {
-            console.log('Connected to the server');
-            isConnected = true;
-            
-            // Отправляем сообщения из очереди
-            while (messageQueue.length > 0) {
-                const message = messageQueue.shift();
-                sendMessage(message);
-            }
+  return new Promise((resolve, reject) => {
+    socket = new WebSocket(SERVER_URL);
 
-            resolve();
-        };
+    socket.onopen = () => {
+      console.log("Connected to the server");
+      isConnected = true;
 
-        socket.onmessage = (message) => {
-            const data = JSON.parse(message.data);
+      // Отправляем сообщения из очереди
+      while (messageQueue.length > 0) {
+        const message = messageQueue.shift();
+        sendMessage(message);
+      }
 
-            for (let i = 0; i < onMessageCallbacks.length; i++) {
-                onMessageCallbacks[i](data);
-            }
-            // if (onMessageCallbacks.length > 0) {
-            //     onMessageCallbacks(data);
-            // }
-            console.log('Message from server: ', message.data);
-        };
+      resolve();
+    };
 
-        socket.onclose = () => {
-            console.log('Disconnected from the server');
-            isConnected = false;
-        };
+    socket.onmessage = (message) => {
+      const data = JSON.parse(message.data);
 
-        socket.onerror = (error) => {
-            console.error('WebSocket error: ', error);
-            isConnected = false;
-            reject(error);
-        };
-    });
+      for (let i = 0; i < onMessageCallbacks.length; i++) {
+        onMessageCallbacks[i](data);
+      }
+      // if (onMessageCallbacks.length > 0) {
+      //     onMessageCallbacks(data);
+      // }
+      console.log("Message from server: ", message.data);
+    };
+
+    socket.onclose = () => {
+      console.log("Disconnected from the server");
+      isConnected = false;
+    };
+
+    socket.onerror = (error) => {
+      console.error("WebSocket error: ", error);
+      isConnected = false;
+      reject(error);
+    };
+  });
 };
 
 export const sendMessage = (message) => {
-    if (isConnected && socket && socket.readyState === WebSocket.OPEN) {
-        socket.send(JSON.stringify(message));
-        console.log('Message sent:', message);
-    } else {
-        console.log('WebSocket is not open. Queueing message:', message);
-        messageQueue.push(message);
-    }
+  if (isConnected && socket && socket.readyState === WebSocket.OPEN) {
+    socket.send(JSON.stringify(message));
+    console.log("Message sent:", message);
+  } else {
+    console.log("WebSocket is not open. Queueing message:", message);
+    messageQueue.push(message);
+  }
 };
 
 export const addOnMessageCallback = (callback) => {
-    if (callback != null) {
-        onMessageCallbacks.push(callback);
-    }
+  if (callback != null) {
+    onMessageCallbacks.push(callback);
+  }
 };
 
 export const setOnDisconnectCallback = (callback) => {
-    onDisconnectCallback = callback;
+  onDisconnectCallback = callback;
 };
 
 export const closeConnection = () => {
-    if (socket) {
-        socket.close();
-    }
-    isConnected = false;
+  if (socket) {
+    socket.close();
+  }
+  isConnected = false;
 };
 
 export const getConnectionStatus = () => {
-    return isConnected;
+  return isConnected;
 };
